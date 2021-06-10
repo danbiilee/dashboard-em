a76b151c5769ede5cf4b712c527465b92646739f
kimbyeongcheol <ditls21@naver.com>
2021-06-10 15:05:10 +0900

    최초커밋

---
+++ src/components/App/AppClient.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./AppClient.module.scss";
+
+const AppClient = () => {
+  return <div className={styles.app}>Hello World!</div>;
+};
+
+export default AppClient;
