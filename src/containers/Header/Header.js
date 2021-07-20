818f63d6baffb658a6b9b9f80124178fa9d4f701
danbiilee <danbi.db@gmail.com>
2021-07-20 13:14:03 +0900

    헤더 Switch 버튼, Dark Mode 적용

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTheme } from "../../context/Theme";
+import { Switch } from "@progress/kendo-react-inputs";
+import "@progress/kendo-theme-bootstrap/dist/all.scss";
 import styles from "./Header.module.scss";
+import "Styles/lib/kendoSwitch.scss";
 
 const Header = () => {
+  const [themeMode, setThemeMode] = useTheme();
+  const [checked, setChecked] = useState(themeMode === "light" ? false : true);
+
+  const handleChangeThemeMode = (e) => {
+    setChecked(e.target.value);
+    setThemeMode();
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.tab}>tab tab2</div>
       <h1 className={styles.logo}>SMS DASHBOARD</h1>
       <div className={styles.clock}>CLOCK</div>
-      <div className={styles.mode}>MODE</div>
+      <div className={styles.mode}>
+        <Switch onChange={handleChangeThemeMode} checked={checked} />
+        Dark Mode
+      </div>
     </header>
   );
 };
