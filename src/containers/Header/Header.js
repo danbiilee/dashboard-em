61bcf125b2dc2c7cf086a002a26d2b2af5e6ae1b
danbiilee <danbi.db@gmail.com>
2021-07-22 10:03:57 +0900

    Header 컴포넌트 UI 수정

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import { useTheme } from "../../context/Theme";
+import { NavLink, useLocation } from "react-router-dom";
+import Clock from "../../components/Clock/Clock";
 import { Switch } from "@progress/kendo-react-inputs";
 import "@progress/kendo-theme-bootstrap/dist/all.scss";
 import styles from "./Header.module.scss";
 import "Styles/lib/kendoSwitch.scss";
 
 const Header = () => {
+  const { pathname } = useLocation();
   const [themeMode, setThemeMode] = useTheme();
   const [checked, setChecked] = useState(themeMode === "light" ? false : true);
 
@@ -16,12 +19,38 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <div className={styles.tab}>tab tab2</div>
-      <h1 className={styles.logo}>SMS DASHBOARD</h1>
-      <div className={styles.clock}>CLOCK</div>
-      <div className={styles.mode}>
-        <Switch onChange={handleChangeThemeMode} checked={checked} />
-        Dark Mode
+      <div className={styles.tab}>
+        <NavLink
+          exact
+          to="/"
+          className={styles.link}
+          activeClassName={styles.selected}
+        >
+          SMS
+        </NavLink>
+        <NavLink
+          to="/nms"
+          className={styles.link}
+          activeClassName={styles.selected}
+        >
+          NMS
+        </NavLink>
+      </div>
+      <h1 className={styles.logo}>
+        {pathname === "/" ? "SMS" : "NMS"} DASHBOARD
+      </h1>
+      <div className={styles.utils}>
+        <div className={styles.clock}>
+          <Clock />
+        </div>
+        <div className={styles.mode}>
+          <Switch
+            className={styles.switch}
+            onChange={handleChangeThemeMode}
+            checked={checked}
+          />
+          <span className={styles.mode__text}>Dark Mode</span>
+        </div>
       </div>
     </header>
   );
