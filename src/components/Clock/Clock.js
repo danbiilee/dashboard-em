ef296ed790ae5f4581472ad8ff7e164966aeede0
danbiilee <danbi.db@gmail.com>
2021-07-22 09:54:21 +0900

    Clock 컴포넌트 수정

--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 
 const Clock = () => {
   const [dateTime, setDateTime] = useState(new Date());
-  const day = ["(일)", "(월)", "(화)", "(수)", "(목)", "(금)", "(토)"];
 
   useEffect(() => {
-    const id = setInterval(() => setDateTime(new Date(), 1000));
+    const id = setInterval(() => setDateTime(new Date()), 5000);
     return () => {
       clearInterval(id);
     };
   }, []);
+
+  const day = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   const getFormattedDate = (dateTime) => {
     let year = dateTime.getFullYear();
     let month = String(dateTime.getMonth() + 1).padStart(2, "0");
@@ -17,9 +18,11 @@ const Clock = () => {
     let hour = String(dateTime.getHours()).padStart(2, "0");
     let minute = String(dateTime.getMinutes()).padStart(2, "0");
     let days = day[String(dateTime.getDay())];
-    return `${year}-${month}-${date} ${days} ${hour}:${minute}`;
+    return `${year}.${month}.${date} ${days} ${hour}:${minute}`;
   };
+
   const formatDate = getFormattedDate(dateTime);
+
   return <div>{formatDate}</div>;
 };
 
