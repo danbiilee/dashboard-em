1bbc0dc15e06b5ad8429249ce0640b3f53520624
danbiilee <danbi.db@gmail.com>
2021-07-30 10:59:38 +0900

    헤더 컬러모드 문구 수정

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -3,9 +3,11 @@ import { NavLink, useLocation } from "react-router-dom";
 import Clock from "../../components/Clock/Clock";
 import ThemeButton from "../../components/ThemeButton/ThemeButton";
 import styles from "./Header.module.scss";
+import { useTheme } from "../../context/Theme";
 
 const Header = () => {
   const { pathname } = useLocation();
+  const themeMode = useTheme();
 
   return (
     <header className={styles.header}>
@@ -35,7 +37,9 @@ const Header = () => {
         </div>
         <div className={styles.mode}>
           <ThemeButton />
-          <span className={styles.mode__text}>Dark Mode</span>
+          <span className={styles.mode__text}>
+            {themeMode[0].toUpperCase() + themeMode.slice(1)} Mode
+          </span>
         </div>
       </div>
     </header>
