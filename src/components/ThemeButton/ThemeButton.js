6070ed4e1521784f6e0d3aba1a3d20d09533ff0b
danbiilee <danbi.db@gmail.com>
2021-08-06 16:27:17 +0900

    그리드 공통 CSS파일 분리

--- src/components/ThemeButton/ThemeButton.js
+++ src/components/ThemeButton/ThemeButton.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useTheme, useThemeToggle } from "../../context/Theme";
 import { Switch } from "@progress/kendo-react-inputs";
 import "@progress/kendo-theme-bootstrap/dist/all.scss";
-import "Styles/lib/kendoSwitch.scss";
 
 const ThemeButton = () => {
   const themeMode = useTheme();
