70af2d7b5b2dae291ef7e9c2712fa7684fd621a4
danbiilee <danbi.db@gmail.com>
2021-08-13 16:35:03 +0900

    RX/TX 전환 적용(#58730)
    - 버튼용 page.module.scss 구조 추가(SMS/NMS 공통)
     - .title__container: 버튼이 들어가는 타이틀 영역의 부모
     - .title__container__button: 버튼의 부모
     - .title__container__button.resource: 버튼 중 리소스 선택 버튼

--- src/pages/NMS/NMSPage.js
+++ src/pages/NMS/NMSPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "../../containers/Main";
 import Top from "../../containers/Main/Top";
 import Title from "../../components/Title";
@@ -8,8 +8,10 @@ import RankGrid from "../../components/RankGrid";
 import styles from "../pages.module.scss";
 import { gridData } from "./TestData";
 import TrafficChart from "../../components/TrafficChart";
+import RTXButtons from "../../components/RTXButtons";
 
 const NMSPage = () => {
+  const [selectedData, setSelectedData] = useState("RX");
   const selectedResourceId = 1;
   const chartGroups = gridData.trafficChartGroupData.find(
     (item) => item.RESOURCE_ID === selectedResourceId
@@ -28,10 +30,17 @@ const NMSPage = () => {
             <Grid />
           </div>
           <div className={styles.side__container}>
-            {/* RX, TX 전환 필요 */}
-            <Title name="트래픽 사용률(RX) TOP5" />
+            <div className={styles.title__container}>
+              <Title name={`트래픽 사용률(${selectedData}) TOP5`} />
+              <div className={`${styles.title__container__button} top5`}>
+                <RTXButtons
+                  selectedData={selectedData}
+                  setSelectedData={setSelectedData}
+                />
+              </div>
+            </div>
             <RankGrid
-              data={gridData.rankTrafficData}
+              data={gridData.rankTrafficData[selectedData]}
               columns={gridData.trafficColumns}
             />
           </div>
@@ -39,6 +48,7 @@ const NMSPage = () => {
         <Top.Center>
           <div className={styles.sms__center__container}>
             <Title name="Topology MAP" />
+            <div className={styles.nms__border__container}></div>
           </div>
         </Top.Center>
         <Top.Side>
@@ -56,7 +66,7 @@ const NMSPage = () => {
         <div className={styles.bottom__container}>
           <Title name="알람콘솔" />
           <AlarmGrid
-            data={gridData.AlarmData}
+            data={gridData.alarmData}
             columns={gridData.alarmColumns}
           />
         </div>
