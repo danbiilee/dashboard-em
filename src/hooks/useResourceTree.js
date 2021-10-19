b7f988f003d607d04b86e8db557a82b2147b88f9
dblee <danbi.db@gmail.com>
2021-10-19 14:39:35 +0900

    #59860 로직 수정

--- src/hooks/useResourceTree.js
+++ src/hooks/useResourceTree.js
@@ -42,57 +42,65 @@ export const useResourceTree = (initialState) => {
       };
 
       const { totalDepth } = state;
-      const acc = Array.from({ length: totalDepth }, () => 0); // checkedChildrenCnt 누적 배열
-      const nests = Array.from({ length: totalDepth }, () => null);
-      const keys = Array.from({ length: totalDepth - 1 }, () => null);
-      let bool = false;
+      const acc = Array.from({ length: totalDepth + 1 }, () => 0); // checkedChildrenCnt 누적
+      const nests = Array.from({ length: totalDepth + 1 }, () => null); // 진행중 노드
+      const keys = Array.from({ length: totalDepth }, () => null); // children 키들
+      let isBreak = false; // 목적지 도달 여부 -> 반복 중단
+      let isAcc = false;
 
-      const search = (lv, key, v) => {
-        if (dest.depth !== 0 && lv === dest.depth && key !== dest.key) {
+      const search = (depth, key, v) => {
+        // 같은 뎁스의 다른 키는 패스(전체 선택 제외)
+        if (dest.depth !== 0 && depth === dest.depth && key !== dest.key) {
           return;
         }
 
-        if (lv === totalDepth) {
-          const nextState = checked;
-          acc[v.depth] += nextState ? 1 : -1;
-          nests[lv - 1].checked = nextState;
+        if (isAcc) {
+          nests[depth - 1].checked = checked; // 체크
+          acc[depth - 1] += checked ? 1 : -1; // 누적
 
           if (key === dest.key) {
-            bool = true;
+            isBreak = true;
           }
-
           return;
         } else {
-          nests[lv] = v;
+          nests[depth] = v;
 
           if (!v.expandable) {
-            search(lv + 1, key, v);
+            isAcc = true;
+            search(depth + 1, key, v);
+            isAcc = false;
             return;
           }
 
-          keys[lv] = Object.keys(v.children);
-
-          while (keys[lv].length) {
-            if (bool) {
+          keys[depth] = Object.keys(v.children);
+          while (keys[depth].length) {
+            if (isBreak) {
               break;
             }
-
-            const childKey = keys[lv].shift();
-            search(lv + 1, childKey, v.children[childKey]);
+            const childKey = keys[depth].shift();
+            search(depth + 1, childKey, v.children[childKey]);
           }
 
           // 체크된 자식들을 기준으로 부모의 cnt, checked 값 설정
-          const childrenDepth = lv + 1;
-          if (lv !== 0 && checked && (key === dest.key || dest.depth === 0)) {
+          const childrenDepth = depth + 1;
+          if (!acc[childrenDepth]) {
+            return;
+          }
+
+          if (v.checkedChildrenCnt + acc[childrenDepth] > v.childrenCnt) {
             acc[childrenDepth] -= v.checkedChildrenCnt;
           }
           let checkedSum = acc[childrenDepth];
-
           v.checkedChildrenCnt += checkedSum;
           v.checked = v.childrenCnt === v.checkedChildrenCnt; // 체크
 
-          acc[lv] += checkedSum; // 누적
-          acc[childrenDepth] = 0; // 초기화
+          // 누적 & 초기화
+          if (checked && (dest.depth === 0 || depth > dest.depth)) {
+            acc[depth] += v.checkedChildrenCnt;
+          } else {
+            acc[depth] += checkedSum;
+          }
+          acc[childrenDepth] = 0;
         }
       };
 
